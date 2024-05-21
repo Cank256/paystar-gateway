563250e6edd53af8de61726f96828c95aae9c6d8
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-21 16:47:43 +0300

    Create the Server and add related dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const helmet = require('helmet')
+const http = require('http')
+const mongo = require('mongoose')
+
+const app = express()
+const port = Bun.env.APP_PORT || 8030
+const dbUri = Bun.env.MONGODB_URI || 'mongodb://localhost:27017/paystar-gateway'
+
+app.use(cors)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(helmet)
+
+const server = http.createServer(app)
+
+mongo.connect(dbUri)
+    .then(() => {
+        console.log('Connected to database')
+        server.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`)
+        })
+    })
+    .catch((err: any) => {
+        console.log('Failed to connect to database', err)
+    })
