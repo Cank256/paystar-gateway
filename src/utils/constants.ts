af8a0440eefe5d74954597e07d04d97e961ac320
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-21 16:51:10 +0300

    Add Constant variables for Error messages and Payment methods

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+
+export const ErrorMessages = {
+    NOT_FOUND: 'Resource Not found',
+    UNAUTHORIZED: 'Unauthorized',
+    FORBIDDEN: 'Forbidden',
+    INTERNAL_SERVER_ERROR: 'Internal server error',
+    BAD_REQUEST: 'Bad request',
+    UNPROCESSABLE_ENTITY: 'Unprocessable entity',
+    WRONG_PAYMENT_METHOD: 'Wrong payment method. Use card or momo',
+}
+
+export const PaymentMethods = {
+    CARD: 'card',
+    MOMO: 'momo',
+}
