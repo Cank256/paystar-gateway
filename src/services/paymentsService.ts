4daa8cb2cbc6844b623a903bd18fbbf8c7af3093
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-25 10:47:44 +0300

    Define and initialise the service URL and secret key for flutterwave in the payment service

--- src/services/paymentsService.ts
+++ src/services/paymentsService.ts
@@ -1,6 +1,16 @@
 
 class PaymentsService {
+    serviceUrl: any
+    secretKey: any
+
+    constructor() {
+        this.serviceUrl = Bun.env.FLUTTERWAVE_API_URL
+        this.secretKey = Bun.env.FLUTTERWAVE_SECRET_KEY
+    }
+
     async initiateMobileMoneyPayment(req: any) {
+
+
         return {
             message: 'Momo Payment Initiated'
         }
