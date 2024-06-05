29cc52402f8524f72f332b8cd8a5917db8134800
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-05 12:30:48 +0300

    Document routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,26 @@
 const routes = require('express').Router()
 
+/**
+ * GET /
+ * Welcome route.
+ * @name Welcome Route
+ * @route {GET} /
+ */
 routes.get('/', (req: any, res: any) => {
+    /**
+     * Responds with a welcome message.
+     * @name Welcome Message
+     * @response {Object} Response object
+     * @response {boolean} success Indicates if the request was successful.
+     * @response {string} message Welcome message.
+     */
     res.status(200).json({
         success: true,
         message: 'Welcome to Paystar Gateway API'
     })
 })
 
+// Mount paymentRoutes under /payments path
 routes.use('/payments', require('./paymentRoutes'))
 
 module.exports = routes
--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,12 +1,43 @@
 const paymentRoutes = require('express').Router()
 const paymentsController = require('../controllers/paymentsController')
 
-paymentRoutes.get('/', paymentsController.getAll)
+/**
+ * GET /payments
+ * Get all payments.
+ * @name Get All Payments
+ * @route {GET} /payments
+ * @handler paymentsController.getAll
+ */
+paymentRoutes.get('/', paymentsController.getAll);
 
-paymentRoutes.post('/', paymentsController.initiate)
+/**
+ * POST /payments
+ * Initiate a new payment.
+ * @name Initiate Payment
+ * @route {POST} /payments
+ * @handler paymentsController.initiate
+ */
+paymentRoutes.post('/', paymentsController.initiate);
 
-paymentRoutes.get('/:paymentRef', paymentsController.getOne) 
+/**
+ * GET /payments/:paymentRef
+ * Get a specific payment by its reference.
+ * @name Get Payment by Reference
+ * @route {GET} /payments/:paymentRef
+ * @param {string} paymentRef The reference ID of the payment.
+ * @handler paymentsController.getOne
+ */
+paymentRoutes.get('/:paymentRef', paymentsController.getOne);
 
-paymentRoutes.post('/:paymentRef/refund', paymentsController.refund)
+/**
+ * POST /payments/:paymentRef/refund
+ * Refund a payment.
+ * @name Refund Payment
+ * @route {POST} /payments/:paymentRef/refund
+ * @param {string} paymentRef The reference ID of the payment to be refunded.
+ * @handler paymentsController.refund
+ */
+paymentRoutes.post('/:paymentRef/refund', paymentsController.refund);
 
-module.exports = paymentRoutes
+// Export the router instance
+module.exports = paymentRoutes;
