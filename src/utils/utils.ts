087d430bd4943175d77db8fd951b715d268eff6c
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-05 12:23:03 +0300

    Document the utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,15 @@
 const { StatusCodes, RequestStatus } = require('./constants')
 const Transaction = require('../models/transactionsModel');
 
+/**
+ * Interface representing a standardized API response.
+ * @interface
+ * @name IResponse
+ * @property {number} code - HTTP status code of the response.
+ * @property {boolean} success - Indicates the success status of the request.
+ * @property {string} message - Descriptive message associated with the response.
+ * @property {any} [data] - Optional data payload included in the response.
+ */
 interface IResponse {
     code: number
     success: boolean
@@ -8,7 +17,20 @@ interface IResponse {
     data?: any
 }
 
+/**
+ * Utils class provides utility functions for creating responses, getting status code messages,
+ * and inserting transaction logs.
+ */
+
 class Utils {
+
+    /**
+     * Creates a response object with the specified code, data, and optional extra information.
+     * @param code The HTTP status code for the response.
+     * @param data The data to include in the response.
+     * @param extraInfo Additional information to append to the response message.
+     * @returns The response object containing code, success status, message, and data.
+     */
     createResponse(code: any, data: any, extraInfo: string = ''):IResponse{
         return {
             code,
@@ -18,6 +40,12 @@ class Utils {
         }
     }
 
+    /**
+     * Retrieves the message associated with the provided HTTP status code.
+     * @param code The HTTP status code.
+     * @param extraInfo Additional information to append to the status message.
+     * @returns The status message corresponding to the provided status code.
+     */
     getStatusCodeMessage(code: number, extraInfo: string): string {
         switch (code) {
             case StatusCodes.OK:
@@ -38,6 +66,12 @@ class Utils {
         }
     }
 
+    /**
+     * Inserts a transaction log into the database.
+     * @param reqDetails Details of the request associated with the transaction.
+     * @param message The message to be logged.
+     * @param response The response object to be logged.
+     */
     async insertTransactionLog(
         reqDetails: any,
         message?: string,
@@ -56,7 +90,8 @@ class Utils {
                 createdAt: new Date(),
                 updatedAt: new Date()
             });
-    
+
+            // Save the transaction in the database
             await newTransaction.save();
         } catch (err: any) {
             console.log(err.message)
